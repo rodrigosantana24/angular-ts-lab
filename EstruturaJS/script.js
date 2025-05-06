06fb0d1b7596f3a18a4fd886befde78ff3dd6177
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-05-06 20:11:32 -0300

    Primeiros passos em JavaScript

---
+++ EstruturaJS/script.js
@@ -0,0 +1,33 @@
+// Numbers
+console.log(typeof 2);
+console.log(typeof 2.0);
+console.log(2);
+// Special Numbers
+console.log(typeof Infinity);
+console.log("string" * 1);
+console.log(typeof NaN);
+// Strings
+console.log("Olá!");
+console.log(typeof 'Teste');
+console.log("Exemplo de quebra de \n linha");
+console.log("Exemplo de espaçamento no \t texto");
+// Concatenação
+console.log("Oi" + " Tudo" + " Bem?");
+// Interpolação
+console.log(`2 + 2 = ${2+2}`);
+// Booleanos
+console.log(true);
+console.log(10 > 5);
+console.log("11" == 11);
+console.log("11" === 11);
+console.log("11" != 11);
+console.log("11" !== 11);
+// Operadores Lógicos
+console.log(true&&true);
+console.log(true&&false);
+console.log(true||false);
+console.log(!true);
+// Empty Values
+console.log(typeof null, typeof undefined);
+console.log(null == false); // false
+console.log(undefined == false); // false
