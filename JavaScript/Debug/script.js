e79f0d1847861df74790405e939755dd51c9affa
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-10-14 18:44:47 -0300

    Debug no JavaScript

---
+++ JavaScript/Debug/script.js
@@ -0,0 +1,17 @@
+"use strict";
+// opa = "teste";
+const opa = "teste";
+
+
+let a = 1;
+let b = 2;
+
+if(a == 1 ){
+    a = b + 2
+}
+
+console.log(a); 
+for (let i=0; i<b; i++){
+    a = a + 2;
+    console.log(a);
+}
