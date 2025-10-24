e9d46efcadccabd277ec36cf608c22734780f8d9
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-10-24 16:04:49 -0300

    Tipos em TypeScript

---
+++ TypeScript/Tipos/index.js
@@ -0,0 +1,11 @@
+// number
+var x = 10;
+console.log(x);
+console.log(typeof x);
+//string
+var firstName = "Rodrigo";
+console.log(firstName.toLocaleUpperCase());
+var lastName = " Santana";
+var fullName = firstName + lastName;
+console.log(fullName);
+console.log(typeof fullName);
---
+++ TypeScript/Tipos/index.ts
@@ -0,0 +1,13 @@
+// number
+let x: number = 10;
+console.log(x);
+console.log(typeof x);
+
+//string
+const firstName: string = "Rodrigo";
+console.log(firstName.toLocaleUpperCase());
+const lastName: string = " Santana";
+
+let fullName = firstName + lastName;
+console.log(fullName);
+console.log(typeof fullName);
