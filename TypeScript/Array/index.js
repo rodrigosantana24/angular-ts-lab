634f21f00b282240828c37ebf3e5bafd278cccf2
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-10-24 17:05:22 -0300

    Arrays em TypeScript

---
+++ TypeScript/Array/index.js
@@ -0,0 +1,8 @@
+// Arrays
+let numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+console.log(numbers[2]);
+const nomes = ["Ana", "Bia", "Carlos"];
+nomes.push("Rodrigo");
+console.log(nomes[3]);
+export {};
---
+++ TypeScript/Array/index.ts
@@ -0,0 +1,12 @@
+// Arrays
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+numbers.push(6);
+console.log(numbers[2]);
+
+const nomes: string[] = ["Ana", "Bia", "Carlos"];
+nomes.push("Rodrigo");
+console.log(nomes[3]);
+
+// outra sintaxe array
+const nums: Array<number> = [10, 20, 30];
