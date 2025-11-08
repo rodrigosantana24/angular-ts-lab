2fa021744f7b4fc70db48759e4251e44833cb3cc
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2025-11-08 19:09:40 -0300

    Arrays em TypeScript

--- TypeScript/Array/index.ts
+++ TypeScript/Array/index.ts
@@ -10,3 +10,18 @@ console.log(nomes[3]);
 
 // outra sintaxe array
 const nums: Array<number> = [10, 20, 30];
+
+
+let valores: Array<number> = [10, 20, 30];
+let palavras: Array<string> = ["TypeScript", "Array"];
+
+
+type Usuario = {
+  nome: string;
+  idade: number;
+};
+
+let usuarios: Usuario[] = [
+  { nome: "João", idade: 20 },
+  { nome: "Maria", idade: 25 },
+];
